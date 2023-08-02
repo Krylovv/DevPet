e7532456eefe5cd79f74f3abb381af00368264c6
Krylovv <kryloff.al@gmail.com>
2023-08-02 16:02:52 +0300

    added Dockerfile and requirements

--- Code/WebApp/WebApp/settings.py
+++ Code/WebApp/WebApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)2w_rl-^i&w4du9_u-4(k$*_dszm%7yz65zkuo#hz3ptlpjo-u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
