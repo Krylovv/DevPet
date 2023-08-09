feac71c2cf7fa6d91f781549638a7bde3957d5e5
Krylovv <kryloff.al@gmail.com>
2023-08-09 12:49:59 +0300

    ci cd test

--- Code/WebApp/gpt_bot/views.py
+++ Code/WebApp/gpt_bot/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 import datetime
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    html = "<html><body>NEW COMMIT TEST It is now %s.</body></html>" % now
     return HttpResponse(html)
